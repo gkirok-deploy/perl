c8f2971acbc106bf1461b38270c7bee1b2e96e34
gkirok <gkirok@gmail.com>
2018-11-08 22:50:33 +0000

    sg: fix ingress

--- terraform/security.tf
+++ terraform/security.tf
@@ -5,28 +5,28 @@ resource "aws_security_group" "gk_sg" {
   vpc_id = "${aws_vpc.default.id}"
 
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["${var.gk_jenkins_ip}"]
   }
 
   ingress {
-    from_port   = 0
+    from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 0
+    from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 0
+    from_port   = 22
     to_port     = 22 
     protocol    = "tcp"
     cidr_blocks = ["${var.gk_access_ip}"]
